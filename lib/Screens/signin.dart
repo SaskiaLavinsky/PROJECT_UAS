f836cd55879a35b5ab44d6ba545bb0e804abb326
Saskia Lavinsky <slavinskuyy@gmail.com>
2023-12-03 05:51:56 +0700

    Firebase Login

--- lib/Screens/signin.dart
+++ lib/Screens/signin.dart
@@ -1,8 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:redwallet/Screens/login.dart';
 import 'package:redwallet/utils/color_utils.dart';
 import 'package:redwallet/widget/navbar.dart'; 
-
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({Key? key}) : super(key: key);
@@ -12,27 +12,92 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+
+  void signUserIn() async {
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          content: Container(
+          height: 50, 
+          width: 25,
+          child: Center(
+            child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Color.fromARGB(255, 74, 3, 3)), 
+              backgroundColor: Color.fromARGB(255, 253, 253, 253), 
+            ),
+          ),
+        ),
+      );
+      },
+    );
+
+    try {
+      
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+
+      
+      await Future.delayed(const Duration(seconds: 3));
+
+      
+      Navigator.pop(context);
+
+      
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => MyBottomNavigationBar()),
+      );
+    } catch (e) {
+      print("Error: $e");
+      Navigator.pop(context); 
+
+      
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(
+              'Error',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 26,
+                color: const Color.fromARGB(255, 134, 9, 0),
+              ),
+            ),
+            content: Text(
+              'There was an error during login. Please try again.',
+              style: TextStyle(fontSize: 18),
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: Text(
+                  'OK',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: const Color.fromARGB(255, 116, 9, 2),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.close),
-               onPressed: () {
-                  Navigator.pushReplacement(
-                    context,
-                     MaterialPageRoute(builder: (context) => LoginPage()),
-                   );
-                },
-            ),
-        ],
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-      ),
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
@@ -53,10 +118,25 @@ class _SignInScreenState extends State<SignInScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(left: 250, top: 20),
+                  child: Positioned(
+                      
+                      child: IconButton(
+                        icon: Icon(Icons.close),
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(builder: (context) => LoginPage()),
+                          );
+                        },
+                      ),
+                    ),
+                ),
                 Column(
                   children: [
                     logoWidget("img/logo.png"),
-                    SizedBox(height: 5), 
+                    SizedBox(height: 5),
                     Text(
                       "Welcome back! You've been missed",
                       style: TextStyle(
@@ -68,6 +148,20 @@ class _SignInScreenState extends State<SignInScreen> {
                   ],
                 ),
                 SizedBox(height: 50),
+                TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                  ),
+                ),
+                SizedBox(height: 30),
                 TextField(
                   controller: _passwordController,
                   obscureText: true,
@@ -82,14 +176,9 @@ class _SignInScreenState extends State<SignInScreen> {
                     contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
                   ),
                 ),
-                SizedBox(height: 20),
+                SizedBox(height: 30),
                 ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) =>  MyBottomNavigationBar()), 
-                  );
-                  },
+                  onPressed: signUserIn, 
                   style: ElevatedButton.styleFrom(
                     backgroundColor: const Color.fromARGB(255, 131, 9, 0),
                     padding: EdgeInsets.symmetric(vertical: 15, horizontal: 50),
@@ -109,13 +198,13 @@ class _SignInScreenState extends State<SignInScreen> {
       ),
     );
   }
-}
 
-Image logoWidget(String imageName) {
-  return Image.asset(
-    imageName,
-    fit: BoxFit.fitWidth,
-    width: 200,
-    height: 200,
-  );
+  Image logoWidget(String imageName) {
+    return Image.asset(
+      imageName,
+      fit: BoxFit.fitWidth,
+      width: 200,
+      height: 200,
+    );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:redwallet/Screens/login.dart';
+import 'package:redwallet/firebase_options.dart';
+import 'Screens/login.dart';
 
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(MyApp());
 }
 
