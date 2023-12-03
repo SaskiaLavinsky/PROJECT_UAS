94c7368a9369ba703af81235c0f755f09d2b2c63
Saskia Lavinsky <slavinskuyy@gmail.com>
2023-12-03 12:12:08 +0700

    History Card2.2

--- lib/Screens/history_card2.dart
+++ lib/Screens/history_card2.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:redwallet/data/biodata.dart';
-
+import 'package:redwallet/widget/navbar.dart';
 
 class HistoryCard2 extends StatelessWidget {
   const HistoryCard2({Key? key}) : super(key: key);
@@ -8,27 +8,23 @@ class HistoryCard2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-      
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(1.0), 
+        child: AppBar(
+          backgroundColor: Color.fromARGB(255, 131, 0, 0),
+          elevation: 0,
+        ),
+      ),
       body: SafeArea(
         child: CustomScrollView(
           slivers: [
             SliverToBoxAdapter(
-              child: SizedBox(height:30, child: _head(context)),
+              child: SizedBox(height:300, child: _head(context)),
             ),
             SliverToBoxAdapter(
   child: Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
-      Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            
-          ],
-        ),
-      ),
       Padding(
         padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
         child: Row(
@@ -97,7 +93,50 @@ class HistoryCard2 extends StatelessWidget {
   Widget _head(BuildContext context){
     return Stack(
           children: [
-        
+            Column(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 240,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 131, 0, 0),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(20),
+                    )
+                  ),
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: 35,
+                        left: 340,
+                        child: ClipRRect(
+                      borderRadius: BorderRadius.circular(7),
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(builder: (context) => MyBottomNavigationBar()),
+                          );
+                        },
+                        child: Container(
+                          height: 40,
+                          width: 40,
+                          color: Color.fromRGBO(188, 8, 8, 1),
+                          child: Icon(
+                            Icons.home,
+                            size: 30,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                      
+                    ],
+                  ),
+                ),
+              ],
+            ),
             
           ],
         );
